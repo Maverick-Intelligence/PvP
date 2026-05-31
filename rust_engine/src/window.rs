e5477ae984d2029d8d175e20f636e75c504efa25
Nvision <nvision@maverick-intelligence.ai>
2026-05-31 12:56:01 +0200

    feat(ffi): create rust c ffi binding (#1)
    * feat(ffi): create binding for c_engine in rust_engine
    
    * docs(ffi): add pkg-config to configure C build in Rust
    
    * feat(ffi): rewrite build.rs using pkg-config to bind glfw3 and gl
    
    * docs(gitignore): edit gitignore comment
    
    * docs(gitignore): write C unsafe functions, Rust struct for C function types, and Rust functions for C functions
    
    * refactor(lib): breakdown lib.rs into sprite.rs and window.rs
    
    * test(unit): test Sprite creation
    
    * test(integration): test simple game loop

---
+++ rust_engine/src/window.rs
@@ -0,0 +1,50 @@
+use std::ffi::CString;
+use std::marker::{PhantomData, PhantomPinned};
+use std::os::raw::c_char;
+
+#[repr(C)]
+pub struct GLFWwindow {
+    _data: [usize; 0],
+    _marker: PhantomData<(*mut usize, PhantomPinned)>,
+}
+
+pub fn create_pvp_window(title: String, width: u32, height: u32) {
+    let c_str = CString::new(title).expect("CString::new failed!");
+    let title_as_c_ptr: *const c_char = c_str.as_ptr();
+
+    unsafe { create_game_window(title_as_c_ptr, width, height) }
+}
+
+unsafe extern "C" {
+    fn create_game_window(title: *const c_char, width: u32, height: u32);
+
+    fn update_game_window();
+
+    fn clear_screen();
+
+    fn window_should_close() -> u8;
+
+    fn get_key(window: *const GLFWwindow, key: i32) -> i32;
+
+    fn get_window() -> *const GLFWwindow;
+}
+
+pub fn update_pvp_window() {
+    unsafe { update_game_window() }
+}
+
+pub fn clear_pvp_window_screen() {
+    unsafe { clear_screen() }
+}
+
+pub fn close_pvp_window() -> u8 {
+    unsafe { window_should_close() }
+}
+
+pub fn get_pvp_key(window: *const GLFWwindow, key: i32) -> i32 {
+    unsafe { get_key(window, key) }
+}
+
+pub fn get_pvp_window() -> *const GLFWwindow {
+    unsafe { get_window() }
+}
