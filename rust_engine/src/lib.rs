20f56a9c45e4f51bede8837c05d4b882cc3eea8c
Nvision <nvision@maverick-intelligence.ai>
2026-06-07 20:54:12 +0200

    refactor(integration-test): finalise test_key_presses with macro loop

--- rust_engine/src/lib.rs
+++ rust_engine/src/lib.rs
@@ -3,23 +3,39 @@ pub mod window;
 
 #[macro_export]
 macro_rules! spawn_sprite {
+<<<<<<< HEAD
     ($x:expr, $y:expr, $w:expr, $h:expr, $r:expr, $g:expr, $b:expr) => {{
         let mut tmp_sprite = $crate::sprite::Sprite::new($x, $y, $w, $h, $r, $g, $b);
         tmp_sprite.render();
         tmp_sprite
     }};
+=======
+    ($x:expr, $y:expr, $w:expr, $h:expr, $r:expr, $g:expr, $b:expr) => {
+        let tmp_sprite = $crate::sprite::create_pvp_sprite($x, $y, $w, $h, $r, $g, $b);
+        $crate::sprite::render_pvp_sprite(tmp_sprite);
+    };
+>>>>>>> 88f77e3 (refactor(integration-test): finalise test_key_presses with macro loop)
 }
 
 #[macro_export]
 macro_rules! move_sprite {
     (clear, $sprite:expr, $x:expr, $y:expr) => {{
         $crate::window::clear_pvp_window_screen();
+<<<<<<< HEAD
         $sprite.update_position($x, $y);
         $sprite.render();
     }};
     (no_clear, $sprite:expr, $x:expr, $y:expr) => {{
         $sprite.update_postition($x, $y);
         $sprite.render();
+=======
+        $crate::sprite::update_pvp_sprite_position($sprite, $x, $y);
+        $crate::sprite::render_pvp_sprite($sprite);
+    }};
+    (no_clear, $sprite:expr, $x:expr, $y:expr) => {{
+        $crate::sprite::update_pvp_sprite_position($sprite, $x, $y);
+        $crate::sprite::render_pvp_sprite($sprite);
+>>>>>>> 88f77e3 (refactor(integration-test): finalise test_key_presses with macro loop)
     }};
 }
 
@@ -49,8 +65,21 @@ macro_rules! on_key_press {
 #[macro_export]
 macro_rules! change_sprite_color {
     ($sprite:expr, $r:expr, $g:expr, $b:expr) => {{
+<<<<<<< HEAD
         $sprite.set_color($r, $g, $b);
         $sprite.render();
+=======
+        let tmp_sprite = $crate::sprite::pvp_sprite_ref($sprite);
+        $crate::spawn_sprite!(
+            tmp_sprite.x,
+            tmp_sprite.y,
+            tmp_sprite.width,
+            tmp_sprite.height,
+            $r,
+            $g,
+            $b
+        );
+>>>>>>> 88f77e3 (refactor(integration-test): finalise test_key_presses with macro loop)
     }};
 }
 
@@ -65,7 +94,11 @@ macro_rules! start_window_and_game_loop {
             if $crate::window::close_pvp_window() == 1 {
                 break;
             }
+<<<<<<< HEAD
             $crate::tick!(10);
+=======
+            $crate::tick!();
+>>>>>>> 88f77e3 (refactor(integration-test): finalise test_key_presses with macro loop)
             $crate::window::clear_pvp_window_screen();
         }
     }};
