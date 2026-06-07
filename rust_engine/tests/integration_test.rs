0acb54e80928b078346849841aec75173242d4b0
Nvision <nvision@maverick-intelligence.ai>
2026-06-07 21:07:01 +0200

    refactor(integration-test): refactor test_simple_game_loop

--- rust_engine/tests/integration_test.rs
+++ rust_engine/tests/integration_test.rs
@@ -2,18 +2,9 @@ extern crate rust_engine;
 
 #[test]
 fn test_simple_game_loop() {
-    let duration = std::time::Duration::from_millis(10);
     let title = String::from("Test Simple Game Loop");
-    rust_engine::window::create_pvp_window(title, 800, 600);
 
-    loop {
-        rust_engine::window::update_pvp_window();
-        if rust_engine::window::close_pvp_window() == 1 {
-            break;
-        }
-        std::thread::sleep(duration);
-    }
-
+    rust_engine::start_window_and_game_loop!(title, 800, 600, rust_engine::window::update_pvp_window(););
     assert_eq!(10, 10);
 }
 
